bf298de4dc44558715996f9c2c0af1290e71572d
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-02-01 16:16:39 +0600

    changed no event found section style

--- src/pages/Dashboard/AllEvents.jsx
+++ src/pages/Dashboard/AllEvents.jsx
@@ -40,11 +40,13 @@ const AllEvents = () => {
                 </div>
             )
                 : (
-                    <div className={styles.dashboardContainer}>
-                        <h1 className={styles.welcomeText}>
-                            Get Started with Your Schedule!
+                    <div className={styles.noEventsContainer}>
+                        <h1 className={styles.noEventsText}>
+                            No events found! <br />
+                            Get Started with Your Schedule <br />
+                            by adding some events
                         </h1>
-                        <div className={styles.welcomeImage}>
+                        <div className={styles.noEventsImage}>
                             <img src="https://images.unsplash.com/photo-1570215171424-f74325192b55?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="a guy scheduling in his computer" />
                         </div>
                     </div>
